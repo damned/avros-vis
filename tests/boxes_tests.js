9d9429ff66fe96497f38dc3bf029ce091e76e8ce
Glitch (aframe) <none>
2021-02-21 17:46:49 +0000

    🐋🎢 Checkpoint ./tests/boxes_tests.js:69342/119

--- tests/boxes_tests.js
+++ tests/boxes_tests.js
@@ -6,14 +6,14 @@ describe('starting a boxes scene', () => {
   const aframeContainer = document.getElementById('aframe-container')
 
   let getScene = () => aframeContainer.querySelector('a-scene')
+  let scene
   
   beforeEach(() => {
-    boxes.startScene('#aframe-container')    
+    boxes.startScene('#aframe-container')
+    scene = getScene()
   })
   
   it('should create a box', (done) => {
-    let scene = getScene()
-    
     scene.addEventListener('renderstart', () => {
       expect(scene.querySelector('a-box')).to.not.be.null
       done()
