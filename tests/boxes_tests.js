dcda2e5f32d4b18433102562c7eec21de15c14f8
Glitch (aframe) <none>
2021-02-21 17:00:27 +0000

    🐲🎮 Checkpoint ./tests/boxes_tests.js:69342/542

--- tests/boxes_tests.js
+++ tests/boxes_tests.js
@@ -1,7 +1,16 @@
-/* global AFRAME createSceneFixture testable */
+/* global AFRAME boxes */
 var chai = chai || {}
 var expect = chai.expect
 
-describe('adding boxes to ', () => {
-  it('shou')
+describe('creating a boxes scene', () => {
+  it('should create a box', () => {
+    let aframeContainer = document.getElementById('aframe-container')
+    
+    boxes.startScene('#aframe-container')
+    
+    let sceneEl = document.querySelector('a-scene')
+    
+    
+    let box
+  })
 })
