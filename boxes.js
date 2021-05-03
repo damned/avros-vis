4ec415190c791c0ae6be0d73c5d6e6fad59e7335
Glitch (aframe) <none>
2021-05-03 19:11:51 +0000

    🍳🍗 Checkpoint ./boxes.js:69342/442 ./index.html:69342/280

--- boxes.js
+++ boxes.js
@@ -1,9 +1,11 @@
-    box.setAttribute('color', 'redvar boxes = {
-  startScene: (sceneContainerSpec) => {
-    let scene = document.createElement('a-scene')
-    document.querySelector(sceneContainerSpec).appendChild(scene)
-    let box     box.setAttribute('color', 'green= document.createElement('a-box')
- )
-    scene.appendChild(box)
-  }
-}
+
+// var boxes = {
+//   startScene: (sceneContainerSpec) => {
+//     let scene = document.createElement('a-scene')
+//     document.querySelector(sceneContainerSpec).appendChild(scene)
+//     let box = document.createElement('a-box')
+//     box.setAttribute('color', 'red')
+//     box.setAttribute('color', 'green')
+//     scene.appendChild(box)
+//   }
+// }
