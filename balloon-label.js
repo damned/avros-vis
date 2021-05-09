d92378f5d24f3adcc9ab550660ac55de5b4a228d
Glitch (aframe) <none>
2021-05-09 19:55:02 +0000

    🙆🏟 Checkpoint ./index.html:69342/185 ./balloon-label.js:69342/881

--- balloon-label.js
+++ balloon-label.js
@@ -1,10 +1,20 @@
 /* globals AFRAME */
 AFRAME.registerComponent('balloon-label', {
   init: function () {
-    console.log('what the heck')
+    let pos = this.el.object3D.position
+    let parent = this.el.object3D.parent.el
+    console.log('labelling element at: ' + JSON.stringify(pos))
+    
     let text = document.createElement('a-text')
+    text.setAttribute('align', 'center')
     text.setAttribute('value', this.data)
-    text.setAttribute('position', '0 1 0')
-    this.el.appendChild(text)
+    text.setAttribute('color', '#888')
+    text.setAttribute('scale')
+    text.setAttribute('position', `${pos.x} ${pos.y + 0.5} ${pos.z}`)
+    parent.appendChild(text)
+    
+    let line = document.createElement('a-entity')
+    line.setAttribute('line', `start: ${pos.x} ${pos.y + 0.5} ${pos.z}; end: ${pos.x} ${pos.y} ${pos.z}`)
+    parent.appendChild(line)
   }
 })
