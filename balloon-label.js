947516afc5e505624410f7ff46a2fa7c7362209b
Glitch (aframe) <none>
2021-05-14 20:33:11 +0000

    🙉💐 Checkpoint ./balloon-label.js:69342/496 ./index.html:69342/79

--- balloon-label.js
+++ balloon-label.js
@@ -2,22 +2,27 @@
 AFRAME.registerComponent('balloon-label', {
   schema: {
     label: { type: "string" },
+    scale: { type: "number", default: 0.5 },
+    yOffset: { type: "number", default: 0 }
   },
   init: function () {
     let pos = this.el.object3D.position
     let parent = this.el.parentNode
     console.log('labelling element at: ' + JSON.stringify(pos))
     
+    
+    let labelY = pos.y + 0.5 + this.data.yOffset
+    
     let text = document.createElement('a-text')
     text.setAttribute('align', 'center')
     text.setAttribute('value', this.data.label)
     text.setAttribute('color', '#888')
-    text.setAttribute('scale', '0.5 0.5')
-    text.setAttribute('position', `${pos.x} ${pos.y + 0.5} ${pos.z}`)
+    text.setAttribute('scale', `${this.data.scale} ${this.data.scale}`)
+    text.setAttribute('position', `${pos.x} ${labelY} ${pos.z}`)
     parent.appendChild(text)
     
     let line = document.createElement('a-entity')
-    line.setAttribute('line', `opacity: 0.3; start: ${pos.x} ${pos.y + 0.5} ${pos.z}; end: ${pos.x} ${pos.y} ${pos.z}`)
+    line.setAttribute('line', `opacity: 0.3; start: ${pos.x} ${labelY} ${pos.z}; end: ${pos.x} ${pos.y} ${pos.z}`)
     parent.appendChild(line)
   }
 })
