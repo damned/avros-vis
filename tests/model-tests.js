06c0271f521bf4152829d042e8ec58c74d372ea6
Glitch (aframe) <none>
2021-05-15 13:50:03 +0000

    ⛵️🍯 Checkpoint ./tests/model-tests.js:69342/1282

--- tests/model-tests.js
+++ tests/model-tests.js
@@ -4,6 +4,7 @@ var expect = chai.expect
 
 describe('model', () => {
   const aframeContainer = document.getElementById('aframe-container')
+  const shape = el => el.components['geometry'].data.primitive
 
   let table, builder
 
@@ -22,6 +23,22 @@ describe('model', () => {
     afterTick(() => {
       let boardEl = document.querySelector('#first-board')
 
+      expect(shape(boardEl)).to.equal('box')
+      expect(boardEl.getAttribute('height')).to.equal('0.1')
+      expect(boardEl.parentNode) == table
+      done()
+    })
+  })
+
+  it('should add a rectangular panel directly onto a board', (done) => {
+    let board = Model(table).board('board')
+    let panel = board.panel('the-panel')
+    
+    afterTick(() => {
+      let boardEl = document.querySelector('#board')
+      let panelEl = document.querySelector('#the-panel')
+
+      expect(shape(boardEl)).to.equal('box')
       expect(boardEl.getAttribute('height')).to.equal('0.1')
       expect(boardEl.parentNode) == table
       done()
