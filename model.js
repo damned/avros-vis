cae32c9741d43ac5332df40e0fcd7dad6f5c8db9
Glitch (aframe) <none>
2021-05-15 14:01:56 +0000

    🏚🌎 Checkpoint ./tests/model-tests.js:69342/478 ./model.js:69342/1237

--- model.js
+++ model.js
@@ -1,15 +1,34 @@
 
+var Panel = function(name, parent) {
+  let api = {}
+  
+  return api
+}
+
+var Board = function(name, parent) {
+  let api = {}
+  
+  let el = document.createElement('a-box')
+  el.setAttribute('id', name)
+  el.setAttribute('height', 0.1)
+  
+  api.panel = (panelName) => {
+    return Panel(panelName)
+  }
+  
+  api.el = el
+
+  return api
+}
+
 var Model = function(foundationEl) {
   let self = this
-  
   let api = {}
   
   api.board = function(name) {
-    let board = document.createElement('a-box')
-    board.setAttribute('id', name)
-    board.setAttribute('height', 0.1)
-    foundationEl.appendChild(board)
-    return api
+    let board = Board(name, foundationEl)
+    foundationEl.appendChild(board.el)
+    return board
   }
   
   return api
--- tests/model-tests.js
+++ tests/model-tests.js
@@ -5,6 +5,8 @@ var expect = chai.expect
 describe('model', () => {
   const aframeContainer = document.getElementById('aframe-container')
   const shape = el => el.components['geometry'].data.primitive
+  const height = el => el.getAttribute('height')
+  const select = selector => document.querySelector(selector)
 
   let table, builder
 
@@ -21,11 +23,12 @@ describe('model', () => {
     let board = Model(table).board('first-board')
     
     afterTick(() => {
-      let boardEl = document.querySelector('#first-board')
+      let boardEl = select('#first-board')
 
       expect(shape(boardEl)).to.equal('box')
       expect(boardEl.getAttribute('height')).to.equal('0.1')
-      expect(boardEl.parentNode) == table
+      expect(boardEl.parentNode).to.equal(table)
+      
       done()
     })
   })
@@ -35,12 +38,12 @@ describe('model', () => {
     let panel = board.panel('the-panel')
     
     afterTick(() => {
-      let boardEl = document.querySelector('#board')
-      let panelEl = document.querySelector('#the-panel')
+      let boardEl = select('#board')
+      let panelEl = select('#the-panel')
 
-      expect(shape(boardEl)).to.equal('box')
-      expect(boardEl.getAttribute('height')).to.equal('0.1')
-      expect(boardEl.parentNode) == table
+      expect(shape(panelEl)).to.equal('box')
+      expect(height(panelEl)).to.equal('0.1')
+      expect(top(panelEl)).to.equal(top(boardEl) + height(panelEl))
       done()
     })
   })
