589c918d5228fd9ebf883c710e49e1ddd4a143b2
Glitch (aframe) <none>
2021-05-21 16:01:21 +0000

    📯🌝 Checkpoint ./placement.js:69342/1094

--- placement.js
+++ placement.js
@@ -10,7 +10,8 @@ AFRAME.registerComponent('placement', {
     
     self.update = () => {
       let on = self.data.on
-      let placed = false
+      let justPlaced = false
+      let emitPlacedNext = false
       
       let placeOn = () => {
         au.catching(() => {
@@ -23,26 +24,41 @@ AFRAME.registerComponent('placement', {
           let host3d = host.object3D
           console.log('on pos: ', JSON.stringify(onPos))
 
-          let onSize = new THREE.Box3().setFromObject(on3d)
-          let hostSize = new THREE.Box3().setFromObject(host3d)
+          let box = new THREE.Box3()
+          let onSize = box.setFromObject(on3d).getSize(new THREE.Vector3())
+          let hostSize = box.setFromObject(host3d).getSize(new THREE.Vector3())
 
           console.log('on size: ', JSON.stringify(onSize))
           console.log('host size: ', JSON.stringify(hostSize))
 
           let pos = onPos.clone()
+          console.log('on pos y', onPos.y)
+          console.log('on size y', onSize.y)
           let newY = onPos.y + (onSize.y / 2) + (hostSize.y / 2)
           console.log('newY', newY)
           pos.setY(newY)
 
           console.log('setting placement to ', JSON.stringify(pos))
 
-          host.setAttribute(au.xyzTriplet(pos))
+          host.setAttribute('position', au.xyzTriplet(pos))
 
           console.log('placement set to ', JSON.stringify(pos))
+
+          justPlaced = true
         })
         
       }
       
+      // self.tick = () => {
+      //   if (justPlaced) {
+      //     justPlaced = false
+      //     emitPlacedNext = true
+      //   }
+      //   else if (emitPlacedNext) {
+      //     emitPlacedNext = false
+      //     host.emit('placed')          
+      //   }
+      // }
       
       console.log('update: host is loaded: ', host.hasLoaded)
       console.log('update: on is loaded: ', on.hasLoaded)
