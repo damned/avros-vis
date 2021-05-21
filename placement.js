2e44dafbcc5b32e55b305f72004f7255b4f06134
Glitch (aframe) <none>
2021-05-21 15:34:53 +0000

    🌇☎️ Checkpoint ./placement.js:69342/2025

--- placement.js
+++ placement.js
@@ -10,23 +10,35 @@ AFRAME.registerComponent('placement', {
     self.update = () => {
       let au = aframeUtils
       let on = self.data.on
+      let placed = false
       
-      let onPos = on.object3D.position
-      console.log('host is loaded: ', host.hasLoaded)
-      console.log('on is loaded: ', on.hasLoaded)
+      let placeOn = () => {
+        console.log('placeOn: host is loaded: ', host.hasLoaded)
+        console.log('placeOn: on is loaded: ', on.hasLoaded)
 
-      console.log('on pos: ', JSON.stringify(onPos))
+        let onPos = on.object3D.position
+        console.log('on pos: ', JSON.stringify(onPos))
+
+        let onSize = new THREE.Box3().setFromObject(on)
+        let hostSize = new THREE.Box3().setFromObject(host)
+
+        console.log('on size: ', JSON.stringify(onSize))
+        console.log('host size: ', JSON.stringify(hostSize))
+
+        let pos = onPos.clone()
+        pos.y = onPos.y + (onSize.y / 2) + (hostSize.y / 2)
+
+        host.setAttribute(au.xyzTriplet(pos))
+
+        console.log('placement set to ', pos)
+        
+      }
       
       
-      let onSize = new THREE.Box3().setFromObject(on)
-      let hostSize = new THREE.Box3().setFromObject(host)
-      
-      let pos = onPos.clone()
-      pos.y = onPos.y + (onSize.y / 2) + (hostSize.y / 2)
-      
-      host.setAttribute(au.xyzTriplet(pos))
-      
-      console.log('placement set to ', pos)
+      console.log('update: host is loaded: ', host.hasLoaded)
+      console.log('update: on is loaded: ', on.hasLoaded)
+
+      on.addEventListener('loaded', placeOn)
     }
   }
 })
