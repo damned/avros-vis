5e89f3afbc906c5c17866f18e8b5220fcacaaa72
Glitch (aframe) <none>
2021-06-13 18:37:51 +0000

    🐔🏨 Checkpoint ./tests/aframe-test-scene.js:69342/2347

--- tests/aframe-test-scene.js
+++ tests/aframe-test-scene.js
@@ -1,9 +1,44 @@
 var aframeTestScene = function() {
-  const scene = {}
   const aframeContainer = document.getElementById('aframe-container')
   let sceneEl = aframeContainer.querySelector('a-scene')
-  if (!sceneEl) {
-    
+
+  const scene = {}
+  
+  scene.recreate = () => {
+    if (resetSceneBeforeEach || aframeContainer.querySelector('a-scene') === null) {
+      aframeContainer.innerHTML = '<a-scene embedded style="height: 300px; width: 600px;"></a-scene>'
+    }
+    sceneEl = select('a-scene')
   }
+
+
+  if (!sceneEl) {
+    scene.recreate()
+  }
+  
+  
+  let inScene = (handler) => {
+    if (sceneEl.renderStarted) {
+      handler(sceneEl)
+    }
+    else {
+      sceneEl.addEventListener('renderstart', () => {
+        handler(sceneEl)
+      })
+    }
+  }
+
+  let resetSceneBeforeEach = false
+
+  let select = selector => document.querySelector(selector)
+  let addHtmlTo = (root, html, selector) => {
+    root.insertAdjacentHTML('afterbegin', html)
+    if (selector) {
+      return select(selector)
+    }
+    return undefined
+  }
+  let addToScene = (html, selector) => addHtmlTo(sceneEl, html, selector)
+
   return scene
 }
