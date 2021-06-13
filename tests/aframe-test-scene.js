1e76c7fcca3c7ab721e10a14ec2899b5c03092d6
Glitch (aframe) <none>
2021-06-13 19:01:05 +0000

    👝👋 Checkpoint ./tests/aframe-test-scene.js:69342/1682 ./tests/balloon-label-tests.js:69342/758

--- tests/aframe-test-scene.js
+++ tests/aframe-test-scene.js
@@ -1,42 +1,44 @@
-var aframeTestScene = function(resetBeforeEach = false) {
+var aframeTestScene = function(recreateOnReset = false) {
   const aframeContainer = document.getElementById('aframe-container')
   let sceneEl = aframeContainer.querySelector('a-scene')
+  let select = selector => document.querySelector(selector)
 
   const scene = {
-    recreate: () => {
-      if (resetBeforeEach || aframeContainer.querySelector('a-scene') === null) {
+    reset: () => {
+      if (recreateOnReset || aframeContainer.querySelector('a-scene') === null) {
         aframeContainer.innerHTML = '<a-scene embedded style="height: 300px; width: 600px;"></a-scene>'
       }
       sceneEl = select('a-scene')
-    }
-  }
-
-  if (!sceneEl) {
-    scene.recreate()
-  }
-  
-  
-  let inScene = (handler) => {
-    if (sceneEl.renderStarted) {
-      handler(sceneEl)
-    }
-    else {
-      sceneEl.addEventListener('renderstart', () => {
+    },
+    within: (handler) => {
+      if (sceneEl.renderStarted) {
         handler(sceneEl)
-      })
-    }
+      }
+      else {
+        sceneEl.addEventListener('renderstart', () => {
+          handler(sceneEl)
+        })
+      }
+    },
+    addHtmlTo: (root, html, selector) => {
+      root.insertAdjacentHTML('afterbegin', html)
+      if (selector) {
+        return select(selector)
+      }
+      return undefined
+    },
+    addHtmlTo: (root, html, selector) => {
+      root.insertAdjacentHTML('afterbegin', html)
+      if (selector) {
+        return select(selector)
+      }
+      return undefined
+    },
+    addHtml: (html, selector) => scene.addHtmlTo(sceneEl, html, selector)
   }
+  scene.inScene = scene.within
 
-
-  let select = selector => document.querySelector(selector)
-  let addHtmlTo = (root, html, selector) => {
-    root.insertAdjacentHTML('afterbegin', html)
-    if (selector) {
-      return select(selector)
-    }
-    return undefined
-  }
-  let addToScene = (html, selector) => addHtmlTo(sceneEl, html, selector)
+  scene.reset()
 
   return scene
 }
