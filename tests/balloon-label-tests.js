d2593558787ae09795655a85d92994003e0c60ee
Glitch (aframe) <none>
2021-06-13 19:01:05 +0000

    👝👋 Checkpoint ./tests/aframe-test-scene.js:69342/1682 ./tests/balloon-label-tests.js:69342/758

--- tests/balloon-label-tests.js
+++ tests/balloon-label-tests.js
@@ -8,35 +8,16 @@ var TOLERANCE = 0.001
 describe('balloon-label component', () => {
   const scene = aframeTestScene()
 
-//   const aframeContainer = document.getElementById('aframe-container')
 //   const select = selector => document.querySelector(selector)
 //   const top = au.world.top
 //   const bottom = au.world.bottom
 //   const width = au.world.width
 //   const pos = el => el.object3D.position
 
-//   let scene, base, host
-
-//   let addToScene = html => scene.insertAdjacentHTML('afterbegin', html)  
+  let host
   
-//   let resetSceneBeforeEach = true
+  beforeEach(() => scene.reset())
 
-//   let recreateScene = () => {
-//     if (resetSceneBeforeEach || aframeContainer.querySelector('a-scene') === null) {
-//       aframeContainer.innerHTML = '<a-scene embedded style="height: 300px; width: 600px;"></a-scene>'
-//     }
-//     scene = select('a-scene')
-//   }
-  
-//   beforeEach(recreateScene)
-
-
-//   beforeEach(() => {
-//     scene.innerHTML = '<a-box id="base" position="0 0.6 -1.2" color="darkgray" height="1">'
-    
-//     base = document.querySelector('#base')
-//     base.innerHTML = ''
-//   })
       
   it('should place the label directly above the host entity', (done) => {
 //     addToScene('<a-box id="host" placement="on: #base">')
