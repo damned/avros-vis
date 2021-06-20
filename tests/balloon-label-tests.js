b54fae47288c6430a8fe56bf93757d9a0d31a3ed
Glitch (avros-vis) <none>
2021-06-20 09:46:35 +0000

    💧👐 Checkpoint ./tests/balloon-label-tests.js:69342/779

--- tests/balloon-label-tests.js
+++ tests/balloon-label-tests.js
@@ -12,7 +12,7 @@ describe('balloon-label component', () => {
 //   const top = au.world.top
 //   const bottom = au.world.bottom
 //   const width = au.world.width
-//   const pos = el => el.object3D.position
+  const pos = el => el.object3D.position
 
   let host
   
@@ -20,15 +20,17 @@ describe('balloon-label component', () => {
 
       
   it('should place the label directly above the host entity', (done) => {
-//     addToScene('<a-box id="host" placement="on: #base">')
-//     host = select('#host')
+    host = scene.addHtml('<a-box id="host" balloon-label="label: oof">', '#host')
     
-//     host.addEventListener('placed', () => {
-//       expect(bottom(host)).to.be.closeTo(top(base), TOLERANCE)
-//       expect(pos(host).x).to.be.closeTo(pos(base).x, TOLERANCE)
-//       expect(pos(host).z).to.be.closeTo(pos(base).z, TOLERANCE)
-//       done()
-//     })
+    scene.within(() => {
+      host.addEventListener('loaded', () => {
+        let 
+        expect(bottom(host)).to.be.closeTo(top(base), TOLERANCE)
+        expect(pos(host).x).to.be.closeTo(pos(base).x, TOLERANCE)
+        expect(pos(host).z).to.be.closeTo(pos(base).z, TOLERANCE)
+      })
+      done()
+    })
   })
   
   it('should move the label directly over the host entity when it moves', (done) => {
