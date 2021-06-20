6be038d59b04e15a58ae86024c6ca08d87fb971a
Glitch (avros-vis) <none>
2021-06-20 10:33:30 +0000

    🚂👖 Checkpoint ./tests/balloon-label-tests.js:69342/2036

--- tests/balloon-label-tests.js
+++ tests/balloon-label-tests.js
@@ -6,24 +6,21 @@ var au = aframeUtils
 var TOLERANCE = 0.001
 
 describe('balloon-label component', () => {
-  const scene = aframeTestScene({recreateOnReset: true})
+  const scene = aframeTestScene({recreateOnReset: false})
   let root
 
   const select = selector => document.querySelector(selector)
-//   const top = au.world.top
-//   const bottom = au.world.bottom
-//   const width = au.world.width
   const pos = el => el.object3D.position
 
   let host
 
   beforeEach(() => scene.reset())
-  beforeEach(() => {
-    root = scene.addRoot('balloon-label')
-  })
       
   it('should place the label directly above the host entity, by default 0.5 above origin', (done) => {
+    root = scene.addRoot('static-label')
+    
     host = root.addHtml('<a-box id="host" height="0.1" balloon-label="label: oof">', '#host')    
+    
     au.onceLoaded(host, () => {
       au.onceLoaded(root.select('a-text'), label => {
         expect(pos(label).x).to.be.closeTo(pos(host).x, TOLERANCE)
@@ -35,7 +32,10 @@ describe('balloon-label component', () => {
   })
   
   it('should place the label directly above the host entity, by specified offset above origin plus 0.5', (done) => {
-    host = root.addHtml('<a-box id="host" height="0.1" balloon-label="label: oof; y-offset: 0.7">', '#host')    
+    root = scene.addRoot('static-offset-label')
+
+    host = root.addHtml('<a-box id="host" height="0.1" balloon-label="label: ouch; y-offset: 0.7">', '#host')    
+    
     au.onceLoaded(host, () => {
       au.onceLoaded(root.select('a-text'), label => {
         expect(pos(label).x).to.be.closeTo(pos(host).x, TOLERANCE)
@@ -47,15 +47,21 @@ describe('balloon-label component', () => {
   })
   
   it('should move the label directly over the host entity when it moves', (done) => {
-//     addToScene('<a-box id="host" placement="on: #base">')
-//     host = select('#host')
+    root = scene.addRoot('dynamic-label')
     
-//     host.addEventListener('placed', () => {
-//       expect(bottom(host)).to.be.closeTo(top(base), TOLERANCE)
-//       expect(pos(host).x).to.be.closeTo(pos(base).x, TOLERANCE)
-//       expect(pos(host).z).to.be.closeTo(pos(base).z, TOLERANCE)
-//       done()
-//     })
+    host = root.addHtml('<a-box id="host" height="0.1" balloon-label="label: boff">', '#host')    
+    au.onceLoaded(host, () => {
+      au.onceLoaded(root.select('a-text'), label => {
+        
+        pos(host).set(1, 2, 3)
+        label.addEventListener('balloonlabel.moved', () => {
+          expect(pos(label).x).to.be.closeTo(1, TOLERANCE)
+          expect(pos(label).z).to.be.closeTo(3, TOLERANCE)
+          expect(pos(label).y).to.be.closeTo(2 + 0.5, TOLERANCE)
+          done()
+        })        
+      })
+    })
   })
 
 })
