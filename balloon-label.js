aeb5113097582482c84cadd3be452f4964889704
Glitch (avros-vis) <none>
2021-06-20 10:56:57 +0000

    🎈🌓 Checkpoint ./balloon-label.js:69342/1321

--- balloon-label.js
+++ balloon-label.js
@@ -1,4 +1,4 @@
-/* globals AFRAME */
+/* globals AFRAME THREE aframeUtils */
 AFRAME.registerComponent('balloon-label', {
   schema: {
     label: { type: "string" },
@@ -6,27 +6,39 @@ AFRAME.registerComponent('balloon-label', {
     yOffset: { type: "number", default: 0 }
   },
   init: function () {
-    let pos = this.el.object3D.position
+    const au = aframeUtils
+    let hostPos = this.el.object3D.position
+    let lastHostPos = new THREE.Vector3()
+    hostPos.copy(lastHostPos)
     let parent = this.el.parentNode
-    console.log('labelling element at: ' + JSON.stringify(pos))
+    console.log('labelling element at: ' + JSON.stringify(hostPos))
     
     
-    let labelY = pos.y + 0.5 + this.data.yOffset
+    let calcLabelPos = (hostPos) => ({
+      x: hostPos.x,
+      y: hostPos.y + 0.5 + this.data.yOffset,
+      z: hostPos.z
+    })
     
     let text = document.createElement('a-text')
+    let hostPosSpec = au.xyzTriplet(calcLabelPos(hostPos))
     text.setAttribute('align', 'center')
     text.setAttribute('value', this.data.label)
     text.setAttribute('color', '#888')
     text.setAttribute('scale', `${this.data.scale} ${this.data.scale}`)
-    text.setAttribute('position', `${pos.x} ${labelY} ${pos.z}`)
+    text.setAttribute('position', hostPosSpec)
     parent.appendChild(text)
     
     let line = document.createElement('a-entity')
-    line.setAttribute('line', `opacity: 0.3; start: ${pos.x} ${labelY} ${pos.z}; end: ${pos.x} ${pos.y} ${pos.z}`)
+    line.setAttribute('line', `opacity: 0.3; start: ${hostPosSpec}; end: ${au.xyzTriplet(hostPos)}`)
     parent.appendChild(line)
     
-    this.el.addEventListener('componentchanged', function (evt) {
-      this.el.emit('balloonlabel.moved', { description: 'Entity has moved to ' + pos });
-    });
+    this.tick = () => {
+      let newHostPos = this.el.object3D.position
+      if (!lastHostPos.equals(newHostPos)) {
+        this.el.emit('balloonlabel.moved', { description: 'Entity has moved to ' + newHostPos })
+        newHostPos.copy(lastHostPos)
+      }
+    }
   }
 })
