5aa47e72cade9cd1dfaa5c22d7cb2bac785e3468
Glitch (avros-vis) <none>
2021-06-27 14:59:25 +0000

    🌑👧 Checkpoint ./tests/chai-aframe-assertions.js:69342/105 ./tests/chai-aframe-assertions-tests.js:69342/267

--- tests/chai-aframe-assertions-tests.js
+++ tests/chai-aframe-assertions-tests.js
@@ -1,1 +1,11 @@
-describe('chai aframe assertions')
+var chai = require('chai')
+  // , aframeAssertions = require('./chai-aframe-assertions')
+  , expect = chai.expect;
+
+// chai.use(aframeAssertions);
+
+describe('chai aframe assertions', () => {
+  it('should pass', () => {
+    
+  })
+})
