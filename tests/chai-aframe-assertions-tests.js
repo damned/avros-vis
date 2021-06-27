2b0a24820f0bd842cc0dd2c62ab0ffcf48dac864
Glitch (avros-vis) <none>
2021-06-27 16:57:51 +0000

    🐡🍭 Checkpoint ./tests/chai-aframe-assertions.js:69342/713 ./tests/chai-aframe-assertions-tests.js:69342/2875

--- tests/chai-aframe-assertions-tests.js
+++ tests/chai-aframe-assertions-tests.js
@@ -1,10 +1,11 @@
-/* global chai aframeTestScene aframeAssertions */
+/* global chai aframeTestScene aframeAssertions aframeUtils */
 var chai = chai || {}
 var expect = chai.expect
 
 chai.use(aframeAssertions());
 
 describe('chai aframe assertions', () => {
+  const au = aframeUtils
   describe('basic mocha and chai understanding', () => {
     it('allows a passing test to be green', () => {
       expect(true).to.be.true
@@ -25,20 +26,44 @@ describe('chai aframe assertions', () => {
     
     describe('occupying', () => {
       beforeEach(() => root = scene.addRoot())
-      it('should pass if two boxes occupy the same space', () => {
-        let aBox = root.entity('a-box')
-        let anotherBox = root.entity('a-box')
-        
-        expect(aBox).to.occupy(anotherBox)
+      it('should pass if two boxes occupy the same space', done => {
+        scene.within(() => {
+          let aBox = root.entity('a-box')
+          let anotherBox = root.entity('a-box')
+
+          au.onceLoaded(anotherBox, () => {
+            expect(aBox).to.occupy(anotherBox)
+            done()
+          })
+        })
       })
 
-      it('should fail if two boxes do not occupy the same space as they are at different positions', () => {
-        let aBox = root.entity('a-box')
-        let anotherBox = root.entity('a-box', { position: '1 0 0' })
-        
-        expect(() => {
-          expect(aBox).to.occupy(anotherBox)
-        }).to.throw(Error, /expected entity to occupy same space as comparison entity.*position.*0 0 0.*1 0 0/)
+      it('should fail if two boxes do not occupy the same space as they are at different positions', done => {
+        scene.within(() => {
+          let aBox = root.entity('a-box')
+          let anotherBox = root.entity('a-box', { position: '1 0 0' })
+
+          au.onceLoaded(anotherBox, () => {
+            expect(() => {
+              expect(aBox).to.occupy(anotherBox)
+            }).to.throw(Error, /expected entity to occupy same space as comparison entity.*position.*0 0 0.*1 0 0/)
+            done()
+          })
+        })
+      })
+
+      it('should fail if two boxes occupy the same space when they are NOT expected to', done => {
+        scene.within(() => {
+          let aBox = root.entity('a-box')
+          let anotherBox = root.entity('a-box')
+
+          au.onceLoaded(anotherBox, () => {
+            expect(() => {
+              expect(aBox).not.to.occupy(anotherBox)
+            }).to.throw(Error, /expected entity not to occupy same space as comparison entity/)
+            done()
+          })
+        })
       })
     })
   })
--- tests/chai-aframe-assertions.js
+++ tests/chai-aframe-assertions.js
@@ -1,18 +1,23 @@
+/* global AFRAME */
 var aframeAssertions = function () {
   return function (chai, utils) {
     let Assertion = chai.Assertion;
     let flag = utils.flag
 
+    const getPosition = (el) => {
+      return AFRAME.utils.coordinates.stringify(el.object3D.position)
+    }
+
     Assertion.addMethod('occupy', function(val, msg) {
       let self = this
       let obj = self._obj
       
-      let actualPosition = obj.getAttribute('position')
-      let expectedPosition = val.getAttribute('position')
+      let actualPosition = getPosition(obj)
+      let expectedPosition = getPosition(val)
       
       self.assert(actualPosition === expectedPosition,
-                 "expected entity to occupy same space as comparison entity - position did not match #{actualPosition} 1 0 0",
-                 "aframe assertions - negative message not yet implemented")
+                 `expected entity to occupy same space as comparison entity - actual position ${actualPosition} did not match expected position ${expectedPosition}`,
+                 `expected entity not to occupy same space as comparison entity - actual position ${actualPosition} did not match expected position ${expectedPosition}`)
     })
   }
 }
