76867ccb1ccde138626aa7c44fd150c2ac5d88d4
Glitch (avros-vis) <none>
2021-06-27 16:46:36 +0000

    🕷⛴ Checkpoint ./tests/chai-aframe-assertions.js:69342/698 ./tests/chai-aframe-assertions-tests.js:69342/6

--- tests/chai-aframe-assertions.js
+++ tests/chai-aframe-assertions.js
@@ -3,10 +3,16 @@ var aframeAssertions = function () {
     let Assertion = chai.Assertion;
     let flag = utils.flag
 
-    Assertion.addMethod('occupy', (val, msg) => {
+    Assertion.addMethod('occupy', function(val, msg) {
       let self = this
       let obj = self._obj
-      self.assert(obj.getAttribute('po'))
+      
+      let actualPosition = obj.getAttribute('position')
+      let expectedPosition = val.getAttribute('position')
+      
+      self.assert(actualPosition === expectedPosition,
+                 "expected entity to occupy same space as comparison entity - position did not match #{actualPosition} 1 0 0",
+                 "aframe assertions - negative message not yet implemented")
     })
   }
 }
