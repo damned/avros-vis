8ffc2b82b56f59ac1e45d9ee0ad2cfc94aa039bc
Glitch (avros-vis) <none>
2021-06-27 16:11:53 +0000

    🌌🚧 Checkpoint ./tests/chai-aframe-assertions-tests.js:69342/561

--- tests/chai-aframe-assertions-tests.js
+++ tests/chai-aframe-assertions-tests.js
@@ -9,13 +9,19 @@ describe('chai aframe assertions', () => {
     expect(true).to.be.true
   })
   
-  describe('in a scene', () => {
-    let scene = aframeTestScene()
+  describe('in scene', () => {
+    let scene, root
+    beforeEach(() => scene = aframeTestScene())
     beforeEach(() => scene.reset())
     
     describe('occupying', () => {
       it('should pass if two boxes occupy the same space', () => {
+        root = scene.addRoot()
         
+        let aBox = root.entity('a-box')
+        let anotherBox = root.entity('a-box')
+        
+        expect(aBox).to.be.occupying(anotherBox)
       })
     })
   })
