bb8e42f35697703e530daaf5ec2281c3c5cc0dae
Glitch (avros-vis) <none>
2022-10-16 14:48:02 +0000

    👩‍❤️‍💋‍👩🐕 Checkpoint ./tests/chai-aframe-assertions-tests.js:69342/1977 ./tests/split-tests.js:69342/4

--- tests/chai-aframe-assertions-tests.js
+++ tests/chai-aframe-assertions-tests.js
@@ -4,11 +4,6 @@ var expect = chai.expect
 
 chai.use(aframeAssertions());
 
-describe('some random describe', () => {
-  it('has a test', () => {
-    expect(true).to.be.true
-  })
-})
 describe('chai aframe assertions', () => {
   const scene = aframeTestScene()
   
@@ -75,85 +70,81 @@ describe('chai aframe assertions', () => {
           })
         })
 
-        it('should fail if two boxes are at the same position but have different sizes due to scale', done => {
-            let aBox = root.entity('a-box')
-            let anotherBox = root.entity('a-box', { scale: '2 2 2' })
+        it('should fail if two boxes are at the same position but have different sizes due to scale', function(done) {
+          root.testing(this)
+          let aBox = root.entity('a-box')
+          let anotherBox = root.entity('a-box', { scale: '2 2 2' })
 
-            au.onceLoaded(anotherBox, () => {
-              expect(() => {
-                expect(aBox).to.occupy(anotherBox)
-              }).to.throw(Error, /expected entity to occupy same space as comparison entity.*size.*1 1 1.*2 2 2/)
-              done()
-            })
+          au.onceLoaded(anotherBox, () => {
+            expect(() => {
+              expect(aBox).to.occupy(anotherBox)
+            }).to.throw(Error, /expected entity to occupy same space as comparison entity.*size.*1 1 1.*2 2 2/)
+            done()
           })
         })
       })
       describe('for multiple entities', () => {
-        it('should pass if two boxes occupy the same space', done => {
-          scene.within(() => {
-            let aBox = root.entity('a-box')
-            let anotherBox = root.entity('a-box')
+        it('should pass if two boxes occupy the same space', function(done) {
+          root.testing(this)
+          let aBox = root.entity('a-box')
+          let anotherBox = root.entity('a-box')
 
-            au.onceLoaded(anotherBox, () => {
-              expect([aBox]).to.occupy([anotherBox])
-              done()
-            })
+          au.onceLoaded(anotherBox, () => {
+            expect([aBox]).to.occupy([anotherBox])
+            done()
           })
         })
         
-        it('should pass if two boxes occupy the same space as two other boxes in same order', done => {
-          scene.within(() => {
-            let boxes = [
-              root.entity('a-box', {position: '0 1 0'}),
-              root.entity('a-box')
-            ]
-            let expectedBoxes = [
-              root.entity('a-box', {position: '0 1 0'}),
-              root.entity('a-box')
-            ]
+        it('should pass if two boxes occupy the same space as two other boxes in same order', function(done) {
+          root.testing(this)
+          let boxes = [
+            root.entity('a-box', {position: '0 1 0'}),
+            root.entity('a-box')
+          ]
+          let expectedBoxes = [
+            root.entity('a-box', {position: '0 1 0'}),
+            root.entity('a-box')
+          ]
 
-            au.onceLoaded(expectedBoxes[1], () => {
-              expect(boxes).to.occupy(expectedBoxes)
-              done()
-            })
+          au.onceLoaded(expectedBoxes[1], () => {
+            expect(boxes).to.occupy(expectedBoxes)
+            done()
           })
         })
 
-        it('should pass if two boxes occupy the same space as two other boxes in a different order', done => {
-          scene.within(() => {
-            let boxes = [
-              root.entity('a-box', {position: '0 1 0'}),
-              root.entity('a-box')
-            ]
-            let expectedBoxes = [
-              root.entity('a-box'),
-              root.entity('a-box', {position: '0 1 0'}),
-            ]
+        it('should pass if two boxes occupy the same space as two other boxes in a different order', function(done) {
+          root.testing(this)
+          let boxes = [
+            root.entity('a-box', {position: '0 1 0'}),
+            root.entity('a-box')
+          ]
+          let expectedBoxes = [
+            root.entity('a-box'),
+            root.entity('a-box', {position: '0 1 0'}),
+          ]
 
-            au.onceLoaded(expectedBoxes[1], () => {
-              expect(boxes).to.occupy(expectedBoxes)
-              done()
-            })
+          au.onceLoaded(expectedBoxes[1], () => {
+            expect(boxes).to.occupy(expectedBoxes)
+            done()
           })
         })
 
-        it('should fail if one of two boxes does not occupy the same space as two other boxes', done => {
-          scene.within(() => {
-            let boxes = [
-              root.entity('a-box', {position: '0 1 0'}),
-              root.entity('a-box')
-            ]
-            let expectedBoxes = [
-              root.entity('a-box'),
-              root.entity('a-box', {position: '0 0 1'}),
-            ]
+        it('should fail if one of two boxes does not occupy the same space as two other boxes', function(done) {
+          root.testing(this)
+          let boxes = [
+            root.entity('a-box', {position: '0 1 0'}),
+            root.entity('a-box')
+          ]
+          let expectedBoxes = [
+            root.entity('a-box'),
+            root.entity('a-box', {position: '0 0 1'}),
+          ]
 
-            au.onceLoaded(expectedBoxes[1], () => {
-              expect(() => {
-                expect(boxes).to.occupy(expectedBoxes)
-              }).to.throw(Error, /expected entity to occupy same space as comparison entity/)
-              done()
-            })
+          au.onceLoaded(expectedBoxes[1], () => {
+            expect(() => {
+              expect(boxes).to.occupy(expectedBoxes)
+            }).to.throw(Error, /expected entity to occupy same space as comparison entity/)
+            done()
           })
         })
         
