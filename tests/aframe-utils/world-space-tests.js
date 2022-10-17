614d4b726d9dba25a37a045abfea259f4d76c4c2
Glitch (avros-vis) <none>
2022-10-17 20:14:31 +0000

    ✈️💾 Checkpoint ./tests/aframe-utils/world-space-tests.js:69342/1352 ./tests/aframe-test-scene.js:69342/54 ./tests.html:69342/782

--- tests/aframe-utils/world-space-tests.js
+++ tests/aframe-utils/world-space-tests.js
@@ -18,65 +18,57 @@ describe('aframe utils space functions', () => {
       
       let vec3 = (x, y, z) => new THREE.Vector3(x, y, z)
       let subject
+      let root
+      
+      beforeEach(() => root = scene.addRoot())
       
       describe('top()', () => {
         it('should get the world y position of the top of a unit box at origin', (done) => {
-          scene.within(() => {
-            subject = scene.addHtml('<a-box id="top-origin"></a-box>', '#top-origin')
-            subject.addEventListener('loaded', () => {
-              expect(au.world.top(subject)).to.equal(0.5)
-              done()
-            })
+          
+          subject = root.addHtml('<a-box id="top-origin"></a-box>', '#top-origin')
+          subject.addEventListener('loaded', () => {
+            expect(au.world.top(subject)).to.equal(0.5)
+            done()
           })
         })
 
         it('should get the top of a unit box at some height', (done) => {
-          scene.within(() => {
-            subject = scene.addHtml('<a-box id="top-high" position="0 2 0"><a-box>', '#top-high')
-            au.tick(() => {
-              expect(au.world.top(subject)).to.equal(2.5)
-              done()
-            })
+          subject = root.addHtml('<a-box id="top-high" position="0 2 0"><a-box>', '#top-high')
+          au.tick(() => {
+            expect(au.world.top(subject)).to.equal(2.5)
+            done()
           })
         })
 
         it('should get the top of a unit box at origin within an entity at some height', (done) => {
-          scene.within(() => {
-            scene.addHtml('<a-entity position="0 3 -3"><a-box></a-entity>')
-            au.tick(() => {
-              expect(au.world.top(select('a-box'))).to.equal(3.5)
-              done()
-            })
+          root.addHtml('<a-entity position="0 3 -3"><a-box></a-entity>')
+          au.tick(() => {
+            expect(au.world.top(root.select('a-box'))).to.equal(3.5)
+            done()
           })
         })
 
         it('should get the top of a half height box at origin within an entity at some height', (done) => {
-          scene.within(() => {
-            scene.addHtml('<a-entity position="0 3 -3"><a-box height="0.5"></a-entity>')
-            au.tick(() => {
-              expect(au.world.top(select('a-box'))).to.equal(3.25)
-              done()
-            })
+          root.addHtml('<a-entity position="0 3 -3"><a-box height="0.5"></a-entity>')
+          au.tick(() => {
+            expect(au.world.top(root.select('a-box'))).to.equal(3.25)
+            done()
           })
         })
 
         it('should get the top of a unit box at origin within a scaled entity at some height', (done) => {
-          scene.within(() => {
-            scene.addHtml('<a-entity position="0 3 -3" scale="2 2 2"><a-box></a-entity>')
-            au.tick(() => {
-              expect(au.world.top(select('a-box'))).to.equal(4)
-              done()
-            })
+          root.addHtml('<a-entity position="0 3 -3" scale="2 2 2"><a-box></a-entity>')
+          au.tick(() => {
+            expect(au.world.top(root.select('a-box'))).to.equal(4)
+            done()
           })
         })
 
         it('should get the top of a unit box at a non-zero height within an entity at some height', (done) => {
-          scene.within(() => {
-            scene.addHtml('<a-entity position="0 3 -3"><a-box position="2 2 2"></a-entity>')
-            au.tick(() => {
-              expect(au.world.top(select('a-box'))).to.equal(5.5)
-              done()
-            })
+          root.addHtml('<a-entity position="0 3 -3"><a-box position="2 2 2"></a-entity>')
+          au.tick(() => {
+            expect(au.world.top(root.select('a-box'))).to.equal(5.5)
+            done()
           })
         })
       })
